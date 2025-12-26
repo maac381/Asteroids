cf6126a603674071ba8140a3cfa2e92dcf4a67e7
maac381 <maac38184@gmail.com>
2025-12-26 23:54:13 +0500

    C: CH2 L4 module completed

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -12,6 +14,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = (clock.tick(60))/1000
+        print(dt)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
